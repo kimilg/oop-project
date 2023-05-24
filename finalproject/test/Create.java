22e323076c5ae07a7b8ef6e130ca95b3ac2e1548
ilgoo.kim <ilgoo.kim@navercorp.com>
2023-05-24 13:23:34 +0900

    create create

---
+++ finalproject/test/Create.java
@@ -0,0 +1,17 @@
+/**
+ * @(#)Create.java 2023. 05. 24
+ *
+ * Copyright 2023 Naver Corp. All rights Reserved.
+ * Naver PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package finalproject.test;
+
+/**
+ * @author Ilgoo.Kim
+ */
+public class Create {
+	private int y;
+	public int cnt() {
+		return y;
+	}
+}
